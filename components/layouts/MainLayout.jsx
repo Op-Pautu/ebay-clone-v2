9629caee7475a5d8061bfb41e452f57d9ec6218f
Op-Pautu <pautuop@gmail.com>
2024-01-24 11:16:34 +0530

    Add initial commit

---
+++ components/layouts/MainLayout.jsx
@@ -0,0 +1,15 @@
+"use client";
+
+import React from "react";
+
+const MainLayout = ({ children }) => {
+  return (
+    <>
+      <div id="MainLayout" className="min-w-[1050px] max-w-[1300px] mx-auto">
+        ML
+      </div>
+    </>
+  );
+};
+
+export default MainLayout;
