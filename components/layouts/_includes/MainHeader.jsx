3bdaa7ebd2a6ab7d6027c1041ef0cabbf7ad5be3
Op-Pautu <pautuop@gmail.com>
2024-01-29 21:53:35 +0530

    Finish search input functionality

--- components/layouts/_includes/MainHeader.jsx
+++ components/layouts/_includes/MainHeader.jsx
@@ -1,9 +1,42 @@
+"use client";
+
+import debounce from "debounce";
 import Image from "next/image";
 import Link from "next/link";
-import React from "react";
+import React, { useState } from "react";
 import { AiOutlineSearch } from "react-icons/ai";
+import { BiLoaderCircle } from "react-icons/bi";
 
 export default function MainHeader() {
+  const [items, setItems] = useState([]);
+  const [isSearching, setIsSearching] = useState(null);
+
+  const handleSearchName = debounce(async (event) => {
+    if (event.target.value == "") {
+      setItems([]);
+      return;
+    }
+    setIsSearching(true);
+
+    try {
+      const res = await fetch(
+        `/api/products/search-by-name/${event.target.value}`
+      );
+      const result = await res.json();
+
+      if (result) {
+        setItems(result);
+        setIsSearching(false);
+        return;
+      }
+
+      setItems([]);
+      setIsSearching(false);
+    } catch (error) {
+      console.log(error);
+      alert(error);
+    }
+  }, 500);
   return (
     <header id="MainHeader" className="border-b">
       <nav className="flex items-center bg-white w-full mx-auto max-w-[1200px]">
@@ -24,10 +57,41 @@ export default function MainHeader() {
               </button>
 
               <input
-                className="w-full pl-3 text-sm placeholder-gray-400 focus:outline-none"
                 type="text"
                 placeholder="Search for anything"
+                onChange={handleSearchName}
+                className="w-full pl-3 text-sm placeholder-gray-400 focus:outline-none"
               />
+
+              {isSearching ? (
+                <BiLoaderCircle className="mr-2 animate-spin" size={22} />
+              ) : null}
+
+              {items.length > 0 ? (
+                <div className="absolute bg-white max-w-[910px] h-auto w-full z-20 left-0 top-12 border p-1">
+                  {items.map((item) => (
+                    <div className="p-1" key={item.id}>
+                      <Link
+                        href={`/product/${item?.id}`}
+                        className="flex items-center justify-between w-full p-1 px-2 cursor-pointer hover:bg-gray-200"
+                      >
+                        <div className="flex items-center">
+                          <img
+                            className="rounded-md"
+                            width={"40"}
+                            src={item?.url + "/40"}
+                          />
+                          <div className="ml-2 truncate">{item.title}</div>
+                        </div>
+
+                        <p className="truncate">
+                          £{(item?.price / 100).toFixed(2)}
+                        </p>
+                      </Link>
+                    </div>
+                  ))}
+                </div>
+              ) : null}
             </div>
 
             <button className="flex items-center bg-blue-600 text-sm font-semibold text-white p-[11px] ml-2 px-14">
